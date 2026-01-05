cb1fa489cb180b101edfc67f3be61bc519c718e3
yashasjayaraj <yashasjayaraj@gmail.com>
2026-01-05 07:59:10 +0000

    Creation of basic Array

---
+++ Arrays/Arraysrev.java
@@ -0,0 +1,28 @@
+public class Arraysrev{
+    public static void main(String[] args){
+        
+        // Normal or One Dimensional Array - Creation 
+        //int[] arrayName = new int[size]
+
+        //example
+        int[] emptyArr = new int[5];
+
+        int[] arrWithValue = {10,20,30};
+
+        int[] arrWithInitializationList = new int[]{10,20,30};
+
+        // Multidimensional Arrays 
+
+        //int[][] arrayName = new int[row][column];
+
+        // Example 
+
+        int[][] multiDimensionalArray = new int[3][4];
+
+        // so i have 3 arrays inside mulltiDimensionalArray and each array is of size 4. 
+
+        int[][] multiDimentionWithValues = {{1,2,3,4},{5,6,7,8}}; // so 2 Arrays inside multiDimentionWithValues Array and each array is of size 4.
+
+        
+    }
+}
